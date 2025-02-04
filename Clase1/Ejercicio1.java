578b99698eeffe8a7cbbe833d97f078514a7643f
Daniel123B <dbanguerav@estudiante.uniajc.edu.co>
2025-02-04 17:22:13 +0000

    Creación del método sumaExtremos

--- Clase1/Ejercicio1.java
+++ Clase1/Ejercicio1.java
@@ -2,8 +2,9 @@ public class Ejercicio1
 {
    public static void main(String[] args) 
    {
-     int[] a = {2,5,8,3,1,10,11};
+     int[] a = {4,8,2,1,6,7};
      System.out.println(sumaParesImparesArreglo(a));
+     System.out.println(sumaExtremosArreglo(a));
    }
     public static String sumaParesImparesArreglo(int[] a)
     {
@@ -22,6 +23,26 @@ public class Ejercicio1
       }
        cad = "La suma de los números pares = " + sumaPares + " suma de los números impares = " + sumaImpares;
        return cad;
+    }
+
+    public static String sumaExtremosArreglo(int[] a)
+    {
+        String cad = "";
+        //Verificamos si el arreglo es par o impar
+        if (a.length%2 == 0) 
+        {
+            //El arreglo es par
+            for(int i = 0; i < a.length/2; i++)
+            {
+              cad+=(a[i] + (a[a.length-1-i]))+ " ";
+            }
+        }
+        else
+        {
+            //El arreglo es impar
+            cad+="Arreglo impar";
+        }
+        return cad;
     }
    
 }
