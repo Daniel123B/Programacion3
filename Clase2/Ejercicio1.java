4c683cf842dbcdbf79894e0f33f3e1588863df80
Daniel123B <dbanguerav@estudiante.uniajc.edu.co>
2025-02-11 17:29:49 +0000

    Se agregó el método de llenar el arreglo de manera aleatoria

--- Clase2/Ejercicio1.java
+++ Clase2/Ejercicio1.java
@@ -5,7 +5,8 @@ public class Ejercicio1 {
         
         // Creacion del objeto op de la clase OperacionesConArreglos
         OperacionesConArreglos op = new OperacionesConArreglos();
-
+        
+        /*
         // Crear un arreglo de enteros
         int[] a = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         
@@ -13,7 +14,17 @@ public class Ejercicio1 {
         System.out.println(op.mostrarElementos(a));
         System.out.println("La suma de los elementos del vector es igual a: "+ op.sumaElementos(a));
         System.out.println("El promedio de los elementos del vector es igual a: "+ op.promedioElementos(a));
+        */
 
+        int n = 5;
+        int[] a = op.llenarArregloAleatorios(n);
+        System.out.println(op.mostrarElementos(a));
+
+        //Probando la generación de números aleatorios entre un rango de valores
+        int min = -10;
+        int max = 20;
+        int randomNum = min + (int)(Math.random() * ((max - min) + 1));
+        System.out.println("Número aleatorio generado entre " + min + " y " + max + ": " + randomNum);
     
     }
 }
