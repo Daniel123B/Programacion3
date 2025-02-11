9a7eee78d98d6b988fbef84ab56ec18433131c61
Daniel123B <dbanguerav@estudiante.uniajc.edu.co>
2025-02-11 16:35:38 +0000

    Creación de la clase OperacionesConArreglos

---
+++ Clase2/Ejercicio1.java
@@ -0,0 +1,17 @@
+package Clase2;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        
+        // Creacion del objeto op de la clase OperacionesConArreglos
+        OperacionesConArreglos op = new OperacionesConArreglos();
+
+        // Crear un arreglo de enteros
+        int[] a = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        
+        // Mostrar los elementos del arreglo
+        System.out.println(op.mostrarElementos(a));
+
+    
+    }
+}
---
+++ Clase2/OperacionesConArreglos.java
@@ -0,0 +1,13 @@
+package Clase2;
+
+public class OperacionesConArreglos {
+
+    public String mostrarElementos(int[] a) {
+        String cad = "";
+        for (int i = 0; i < a.length; i++) {
+            cad += "a[ " + i + " ] = " + a[i] + "\n";
+        }
+        return cad;
+
+    }
+}
