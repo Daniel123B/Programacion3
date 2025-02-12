2fb2269e38a1017847cb9e2ee695b39846e154e8
Daniel123B <dbanguerav@estudiante.uniajc.edu.co>
2025-02-12 21:53:23 +0000

    Se realizó el segundo ejercicio del taller

--- Clase2/Ejercicio1Talller.java
+++ Clase2/Ejercicio1Talller.java
@@ -6,7 +6,7 @@ public class Ejercicio1Talller {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Ejercico #1 Hallar la suma de los N numeros del vector
+        /*  Ejercico #1 Hallar la suma de los N numeros del vector
 
         VectorEntero vector1 = new VectorEntero();
         System.out.print("Digite el tamaño del vector ");
@@ -16,6 +16,47 @@ public class Ejercicio1Talller {
         vector1.imprimirVertical();
 
         System.out.print("La suma de los elementos del vector es:  " + vector1.sumaElementos());
+        */
+
+        // Ejercicio #2
+
+        VectorEntero vectorSueldo = new VectorEntero();
+        VectorEntero vectorAsignaciones = new VectorEntero();
+        VectorEntero vectorDeducciones = new VectorEntero();
+        VectorEntero vectorNeto = new VectorEntero();
+
+        System.out.print("Digite el número de empleados ");
+        int n = sc.nextInt();
+        vectorSueldo.set(n);
+        vectorSueldo.llenadoAutomatico(200 * n, 500 * n);
+        vectorAsignaciones.set(n);
+        vectorAsignaciones.llenadoAutomatico(200 * n, 500 * n);
+        vectorDeducciones.set(n);
+        vectorDeducciones.llenadoAutomatico(200 * n, 500 * n);
+        vectorNeto.set(n);
+        
+
+        for (int i = 0; i < n; i++) {
+            int sueldo = vectorSueldo.getVector(i);
+            int asignaciones = vectorAsignaciones.getVector(i);
+            int deducciones = vectorDeducciones.getVector(i);
+            int neto = sueldo + asignaciones - deducciones;
+
+            System.out.println("Empleado " + (i + 1) + ":");
+            System.out.println("  Sueldo: " + sueldo);
+            System.out.println("  Asignaciones: " + asignaciones);
+            System.out.println("  Deducciones: " + deducciones);
+            System.out.println("  Neto a pagar: " + neto);
+        }
+    
+
+
+        
+
+
+
+
+
 
     }
 }
--- Clase2/VectorEntero.java
+++ Clase2/VectorEntero.java
@@ -29,6 +29,10 @@ public class VectorEntero {
         }
     }
 
+    public int getVector(int nx){
+        return  v[nx];
+    }
+
     private int aleatorio(int min, int max) {
         return (int) (Math.random() * (max - min + 1) + min);
     }
