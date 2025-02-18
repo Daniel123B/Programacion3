74dcd0d3652389e8c54a64d1d3deb8af0a9f275b
Daniel123B <dbanguerav@estudiante.uniajc.edu.co>
2025-02-18 17:16:37 +0000

    Se realizo un ejemplo de matrices

---
+++ Clase3/EjemploMatrices.java
@@ -0,0 +1,17 @@
+public class EjemploMatrices {
+    public static void main(String[] args) {
+
+        //Declaracion de una matriz 3x3
+
+        int[][]m = {{9,3,5},
+                 {1,4,2},
+                 {8,7,6}} ;
+        
+        for(int i = 0; i < m.length; i++){
+            for(int j = 0; j < m[0].length; j++){
+                System.out.println("m["+i+"]["+j+"]="+m[i][j]);
+            }
+        }
+
+    }
+}
