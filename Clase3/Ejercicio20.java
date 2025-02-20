95d72e97549b7f19884606f36e948699ebc1afa9
Daniel123B <dbanguerav@estudiante.uniajc.edu.co>
2025-02-20 04:02:29 +0000

    Se creo una nueva clase y se pasaron datos de la clase MatrizTabla a la nueva

---
+++ Clase3/Ejercicio20.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Ejercicio20 {
+    public static void main(String[] args) {
+        Scanner  sc = new Scanner(System.in);
+        MatrizCeros matriz20 = new MatrizCeros();
+
+        matriz20.cerosdiagonal();
+    }
+}
--- Clase3/MatrizTabla.java
+++ Clase3/MatrizTabla.java
@@ -10,16 +10,6 @@ public class MatrizTabla {
         n = 10;
         x = 10;
         v = new int[n][x];
-        llenadoAutomatico(n, x);
-    }
-
-    public void llenadoAutomatico(int minimo, int maximo) {
-        Random rand = new Random();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < x; j++) {
-                v[i][j] = rand.nextInt(maximo - minimo + 1) + minimo;
-            }
-        }
     }
 
     public void llenarTabla(){
@@ -30,16 +20,6 @@ public class MatrizTabla {
         }
     }
 
-    public void cerosdiagonal(){
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < x; j++) {
-                if (j >= i) {
-                    v[i][j] = 0;
-                }
-            }
-        }
-    }
-
     public void imprimirMatriz() {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < x; j++) {
@@ -47,18 +27,6 @@ public class MatrizTabla {
             }
         }
     }
-
-    public void mostrarMatriz() {
-        System.out.println("\nLa matriz es: ");
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < x; j++) {
-                System.out.print(v[i][j] + " ");
-            }
-            System.out.println("");
-        }
-    }
-
-}
 
     public void mostrarMatrizTabla() {
         System.out.println("\nLa Tabla de multiplicar del 1 al 10 es: ");
