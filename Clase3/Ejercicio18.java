05101b4126fca69dcbc96d8a42afa6791f79ed93
Daniel123B <dbanguerav@estudiante.uniajc.edu.co>
2025-02-20 02:50:36 +0000

    Se realizo el ejercicio 18, creando una clase nueva y utilizando métodos

---
+++ Clase3/Ejercicio18.java
@@ -0,0 +1,8 @@
+public class Ejercicio18 {
+    public static void main(String[] args) {
+
+        MatrizTabla matriz1 = new MatrizTabla();
+        matriz1.llenarTabla();
+        matriz1.mostrarMatrizTabla();
+    }
+}
---
+++ Clase3/MatrizTabla.java
@@ -0,0 +1,32 @@
+public class MatrizTabla {
+
+    private int[][] v;
+    private int n;
+    private int x;
+
+    public MatrizTabla() {
+        n = 10;
+        x = 10;
+        v = new int[n][x];
+    }
+
+    public void llenarTabla(){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < x; j++) {
+           v[i][j] = (i+1)*(j+1);
+            }  
+        }
+    }
+
+    public void mostrarMatrizTabla() {
+        System.out.println("\nLa Tabla de multiplicar del 1 al 10 es: ");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < x; j++) {
+                System.out.print(String.format("%3d ",v[i][j]));
+            }
+            System.out.println("");
+        }
+    }
+
+
+}
