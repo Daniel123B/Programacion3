be8eb719b5a2624d1ec66bc6cff9f8c5777966e6
Daniel123B <dbanguerav@estudiante.uniajc.edu.co>
2025-02-20 03:56:13 +0000

    Se agregaron métodos a la clase MatrizTabla

--- Clase3/MatrizTabla.java
+++ Clase3/MatrizTabla.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class MatrizTabla {
 
     private int[][] v;
@@ -8,6 +10,16 @@ public class MatrizTabla {
         n = 10;
         x = 10;
         v = new int[n][x];
+        llenadoAutomatico(n, x);
+    }
+
+    public void llenadoAutomatico(int minimo, int maximo) {
+        Random rand = new Random();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < x; j++) {
+                v[i][j] = rand.nextInt(maximo - minimo + 1) + minimo;
+            }
+        }
     }
 
     public void llenarTabla(){
@@ -17,6 +29,36 @@ public class MatrizTabla {
             }  
         }
     }
+
+    public void cerosdiagonal(){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < x; j++) {
+                if (j >= i) {
+                    v[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    public void imprimirMatriz() {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < x; j++) {
+                System.out.println("m[" + i + "][" + j + "]=" + v[i][j]);
+            }
+        }
+    }
+
+    public void mostrarMatriz() {
+        System.out.println("\nLa matriz es: ");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < x; j++) {
+                System.out.print(v[i][j] + " ");
+            }
+            System.out.println("");
+        }
+    }
+
+}
 
     public void mostrarMatrizTabla() {
         System.out.println("\nLa Tabla de multiplicar del 1 al 10 es: ");
