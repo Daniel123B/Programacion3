8d4ae1a506ccf0be85d31c37e789d43a49f6ac4a
Daniel123B <dbanguerav@estudiante.uniajc.edu.co>
2025-04-01 16:01:49 +0000

    Se creo la clase Trabajador

---
+++ Clase9/Trabajador.java
@@ -0,0 +1,17 @@
+public class Trabajador{
+    private String cedula;
+    private String nombre;
+    private double salario;
+
+public Trabajador(String cedula,String nombre, double salario){
+    this.cedula = cedula;
+    this.nombre = nombre;
+    this.salario = salario;
+}
+
+public void Pagar(){
+    System.out.println();
+}
+
+
+}
