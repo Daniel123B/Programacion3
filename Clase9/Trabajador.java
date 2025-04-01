97a6d83952b0dc7b2797e9de87189a6df534fbfc
Daniel123B <dbanguerav@estudiante.uniajc.edu.co>
2025-04-01 16:26:35 +0000

    Se agregarón métodos importantes para la clase Trabajador: Métodos Get, toString y pagar.

--- Clase9/Trabajador.java
+++ Clase9/Trabajador.java
@@ -1,17 +1,32 @@
-public class Trabajador{
+public class Trabajador {
     private String cedula;
     private String nombre;
     private double salario;
 
-public Trabajador(String cedula,String nombre, double salario){
-    this.cedula = cedula;
-    this.nombre = nombre;
-    this.salario = salario;
-}
+    public Trabajador(String cedula, String nombre, double salario) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.salario = salario;
+    }
 
-public void Pagar(){
-    System.out.println();
-}
+    public String getCedula() {
+        return cedula;
+    }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public String toString() {
+        return "Trabajador { cedula: " + cedula + " Nombre: " + nombre + " Salario: " + salario + "}";
+    }
+
+    public double pagar(double incremento){
+        return salario + (salario * incremento);
+    }
 
 }
