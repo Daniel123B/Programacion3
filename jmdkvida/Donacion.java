4e489d0ba95d6b17e9af0be3c731de7e24390c32
Daniel123B <dbanguerav@estudiante.uniajc.edu.co>
2025-05-06 01:40:04 +0000

    Se agrego la clase Donacion

---
+++ jmdkvida/Donacion.java
@@ -0,0 +1,53 @@
+package jmdkvida;
+import java.util.Date;
+
+public class Donacion {
+    private Usuario usuario;
+    private double monto;
+    private Date fecha;
+    
+    // Constructor
+    public Donacion(Usuario usuario, double monto) {
+        this.usuario = usuario;
+        this.monto = monto;
+        this.fecha = new Date();
+    }
+    
+    // Getters y setters
+    public Usuario getUsuario() {
+        return usuario;
+    }
+    
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+    
+    public double getMonto() {
+        return monto;
+    }
+    
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+    
+    public Date getFecha() {
+        return fecha;
+    }
+    
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+    
+    // Método para generar comprobante
+    public String generarComprobante() {
+        String comprobante = "Comprobante de donación\n";
+        comprobante += "Fecha: " + fecha + "\n";
+        comprobante += "Donante: " + usuario.getNombre() + "\n";
+        comprobante += "Monto: $" + monto + "\n";
+        comprobante += "ID de transacción: " + System.currentTimeMillis();
+        
+        System.out.println("Comprobante generado para donación de " + usuario.getNombre());
+        return comprobante;
+    }
+}
+
